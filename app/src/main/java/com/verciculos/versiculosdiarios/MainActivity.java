a57ed3e933ccc011fd575349e170a1794bd795d2
davi1701 <davi.spereira1@gmial.com>
2022-04-07 23:17:41 -0300

    Versiculos Diários

--- app/src/main/java/com/verciculos/versiculosdiarios/MainActivity.java
+++ app/src/main/java/com/verciculos/versiculosdiarios/MainActivity.java
@@ -3,12 +3,25 @@ package com.verciculos.versiculosdiarios;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView verciculos;
+    private Button gerar_verciculos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        verciculos = findViewById(R.id.verciculos);
+        gerar_verciculos = findViewById(R.id.gerar_verciculos);
+
+        getSupportActionBar().hide();
+
+
     }
 }
